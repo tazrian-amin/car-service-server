5554ebb8241c67fb0d6e61eb442aa9b994390243
tazrian-amin <amin.tazrian1979@gmail.com>
2022-11-18 14:16:01 +0600

    included comments

--- index.js
+++ index.js
@@ -29,43 +29,19 @@ function verifyJWT(req, res, next) {
     })
 }
 
-// function verifyJWT(req, res, next) {
-//     const authHeader = req.headers.authorization;
-
-//     if (!authHeader) {
-//         res.status(401).send({ message: 'Unauthorized access!' })
-//     }
-
-//     const token = authHeader.split(' ')[1];
-
-//     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
-//         if (err) {
-//             res.status(401).send({ message: 'Unauthorized access!' })
-//         }
-//         req.decoded = decoded;
-//         next();
-//     })
-// }
-
 async function run() {
     try {
         const serviceCollection = client.db('car').collection('services');
         const orderCollection = client.db('car').collection('orders');
 
+        // Assigning JWT token
         app.post('/jwt', (req, res) => {
             const user = req.body;
             const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
             res.send({ token });
         })
 
-        // app.post('/jwt', (req, res) => {
-        //     const user = req.body;
-        //     const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
-        //     res.send({ token });
-        // })
-
         // READING / getting all data from db to server 
-
         app.get('/services', async (req, res) => {
             const query = {};
             const cursor = serviceCollection.find(query);
@@ -83,30 +59,33 @@ async function run() {
 
         // Orders API
         app.get('/orders', verifyJWT, async (req, res) => {
-            const decoded = req.decoded;
 
+            // JWT verification
+            const decoded = req.decoded;
             if (decoded.email !== req.query.email) {
                 res.status(403).send({ message: 'Forbidden Access' })
             }
 
+            // Reading / getting data from db to server
             let query = {};
             if (req.query.email) {
                 query = {
                     email: req.query.email
                 }
             }
-
             const cursor = orderCollection.find(query);
             const orders = await cursor.toArray();
             res.send(orders);
         })
 
+        // Creating / posting order data to db
         app.post('/orders', verifyJWT, async (req, res) => {
             const order = req.body;
             const result = await orderCollection.insertOne(order);
             res.send(result);
         })
 
+        // Updating order data to db
         app.patch('/orders/:id', verifyJWT, async (req, res) => {
             const id = req.params.id;
             const status = req.body.status;
@@ -120,12 +99,14 @@ async function run() {
             res.send(result);
         })
 
+        // Deleing order data from db
         app.delete('/orders/:id', verifyJWT, async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
             const result = await orderCollection.deleteOne(query);
             res.send(result);
         })
+
     }
     finally {
 
@@ -134,6 +115,7 @@ async function run() {
 
 run().catch(err => console.error(err));
 
+// Default server API
 app.get('/', (req, res) => {
     res.send('Car Server Running Well')
 });
